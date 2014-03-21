62b0c0ad946a7fcd8edc4e4639a7fd4a85d7760b
Sefa <Sefa@HACKER>
2014-03-21 10:42:25 +0100

    Surprise maaaaaan

---
+++ src/oeztuerk/Hello_World.java
@@ -0,0 +1,9 @@
+package oeztuerk;
+
+public class Hello_World {
+	public static void main(String[]args){
+		Hello_World2.start();
+		Hello_World2.hallo1();
+		Hello_World2.hallo2();
+	}
+}
---
+++ src/oeztuerk/Hello_World2.java
@@ -0,0 +1,13 @@
+package oeztuerk;
+
+public class Hello_World2 {
+	public static void start(){
+		System.out.println("Hallo Welt");
+	}
+	public static void hallo1(){
+		System.out.println("Hallo Welt2");
+	}
+	public static void hallo2(){
+		System.out.println("Hallo Welt3");
+	}
+}
